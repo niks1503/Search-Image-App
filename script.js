f94eebf13afef599b0d87b3ffb15c4c556c2ce02
niks1503 <niksharma1503@gmail.com>
2024-11-16 15:41:29 +0545

    updated

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const showMore_btn = document.querySelector("#showMore_btn");
 const accessKey = "JpdOLI233Qb8Nvh3PTZmvXSaB5CAtVlj3V4ZkoZi0JM";
 let inputData = "";
 let page = 1;
+
 async function searchImages() {
   inputData = search.value.toLowerCase();
 
@@ -13,10 +14,17 @@ async function searchImages() {
   const data = await res.json();
   console.log(data);
   data.results.forEach((val) => {
+    const img_div = document.createElement("div");
+    img_div.classList.add("image_div");
+    const title_h4 = document.createElement("h4");
+    title_h4.classList.add("title_h4");
+    title_h4.textContent = val.alt_description;
     const img = document.createElement("img");
     img.alt = val.alt_description;
     img.src = val.urls.regular;
-    images_container.appendChild(img);
+    img_div.appendChild(img);
+    img_div.appendChild(title_h4);
+    images_container.appendChild(img_div);
   });
 }
 
